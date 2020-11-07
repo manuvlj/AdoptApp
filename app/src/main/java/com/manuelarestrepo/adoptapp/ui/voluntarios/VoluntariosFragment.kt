534b610f74413b018c66e30f8df7656dc77caf84
manuvlj <mrc78613@gmail.com>
2020-11-06 22:56:38 -0500

    Creación de interfaces para voluntarios y dar en adopción.

--- app/src/main/java/com/manuelarestrepo/adoptapp/ui/voluntarios/VoluntariosFragment.kt
+++ app/src/main/java/com/manuelarestrepo/adoptapp/ui/voluntarios/VoluntariosFragment.kt
@@ -9,7 +9,6 @@ import com.manuelarestrepo.adoptapp.R
 
 class VoluntariosFragment : Fragment() {
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,5 +17,4 @@ class VoluntariosFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_voluntarios, container, false)
     }
 
-    companion object
 }
