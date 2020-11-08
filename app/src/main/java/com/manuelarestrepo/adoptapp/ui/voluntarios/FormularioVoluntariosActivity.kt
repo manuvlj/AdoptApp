fc02731d7aba9e012187d34798cfffc45182af8a
manuvlj <mrc78613@gmail.com>
2020-11-07 20:38:16 -0500

    Formulario voluntarios

---
+++ app/src/main/java/com/manuelarestrepo/adoptapp/ui/voluntarios/FormularioVoluntariosActivity.kt
@@ -0,0 +1,12 @@
+package com.manuelarestrepo.adoptapp.ui.voluntarios
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.manuelarestrepo.adoptapp.R
+
+class FormularioVoluntariosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_voluntarios)
+    }
+}
