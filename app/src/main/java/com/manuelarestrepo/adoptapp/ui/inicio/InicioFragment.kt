4b710a6a4b04ecfa2e04df11febfa734bac4314c
manuvlj <mrc78613@gmail.com>
2020-11-25 12:20:13 -0500

    Creación de interfaz para usuario e inicio

--- app/src/main/java/com/manuelarestrepo/adoptapp/ui/inicio/InicioFragment.kt
+++ app/src/main/java/com/manuelarestrepo/adoptapp/ui/inicio/InicioFragment.kt
@@ -9,7 +9,6 @@ import com.manuelarestrepo.adoptapp.R
 
 class InicioFragment : Fragment() {
 
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
